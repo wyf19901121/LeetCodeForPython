d9cfc3c18e6aef05a8a2fcbe507d581e15b12ae4
wyf19901121 <wyf19901121@163.com>
2018-10-09 21:04:56 +0800

    add MonotonicArray896

---
+++ MonotonicArray896.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isMonotonic(self, A):
+        """
+        :type A: List[int]
+        :rtype: bool
+        """
+
+        return self.isIncrease(A) or self.isDecrease(A)
+
+    def isIncrease(self, A):
+        return all(A[i] - A[i+1] >= 0 for i in range(len(A) - 1))
+
+    def isDecrease(self, A):
+        return all(A[i] - A[i+1] <= 0 for i in range(len(A) - 1))
