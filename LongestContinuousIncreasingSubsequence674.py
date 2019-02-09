fd6adb616624ac849ef474dbf799eabc9610bf04
wyf19901121 <wyf19901121@163.com>
2019-02-09 10:00:37 +0800

    update 674. Longest Continuous Increasing Subsequence

--- LongestContinuousIncreasingSubsequence674.py
+++ LongestContinuousIncreasingSubsequence674.py
@@ -6,10 +6,8 @@ class Solution:
         """
         maxLen = 0
         curLen = 1
-        if len(nums) == 0:
+        if nums is None or len(nums) == 0:
             return maxLen
-        if len(nums) == 1:
-            return curLen
         for i in range(1, len(nums)):
             if nums[i] <= nums[i - 1]:
                 maxLen = max(curLen, maxLen)
